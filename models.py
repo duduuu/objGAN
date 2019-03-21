93e8cbcd2ef53cc0e4f869a135066383ea6b0d06
duduuu <engus367@naver.com>
2019-03-21 14:17:23 +0900

    Update models.py

--- models.py
+++ models.py
@@ -29,26 +29,26 @@ def generator(x, isTrain=True, reuse=False):
         return output
 
 def discriminator(x, isTrain=True, reuse=False):
-    with tf.variable_scope('discriminator', reuse=reuse):
+	with tf.variable_scope('discriminator', reuse=reuse):
         
-        # 1st hidden layer
-        conv0 = tf.layers.conv2d(x, 128, [4, 4], strides=(2, 2), padding='same')
-        lrelu0 = lrelu(conv0, 0.2)
+		# 1st hidden layer
+		conv0 = tf.layers.conv2d(x, 128, [4, 4], strides=(2, 2), padding='same')
+		lrelu0 = lrelu(conv0, 0.2)
 
         # 2nd hidden layer
-        conv1 = tf.layers.conv2d(lrelu0, 256, [4, 4], strides=(2, 2), padding='same')
-        lrelu1 = lrelu(tf.layers.batch_normalization(conv1, training=isTrain), 0.2)
+		conv1 = tf.layers.conv2d(lrelu0, 256, [4, 4], strides=(2, 2), padding='same')
+		lrelu1 = lrelu(tf.layers.batch_normalization(conv1, training=isTrain), 0.2)
 
         # 3rd hidden layer
-        conv2 = tf.layers.conv2d(lrelu1, 512, [4, 4], strides=(2, 2), padding='same')
-        lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
+		conv2 = tf.layers.conv2d(lrelu1, 512, [4, 4], strides=(2, 2), padding='same')
+		lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
 
         # 4th hidden layer
-        conv3 = tf.layers.conv2d(lrelu2, 1024, [4, 4], strides=(2, 2), padding='same')
-        lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
+		conv3 = tf.layers.conv2d(lrelu2, 1024, [4, 4], strides=(2, 2), padding='same')
+		lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
 
         # output layer
-        conv4 = tf.layers.conv2d(lrelu3, 1, [4, 4], strides=(1, 1), padding='valid')
-        output = tf.nn.sigmoid(conv4)
-
-        return output, conv4
+		conv4 = tf.layers.conv2d(lrelu3, 1, [4, 4], strides=(1, 1), padding='valid')
+		output = tf.nn.sigmoid(conv4)
+		
+		return output, conv4
