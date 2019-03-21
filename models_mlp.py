2779cb7e8cdf1bafb7acdcddcc5f4ec872d73b3b
duduuu <engus367@naver.com>
2019-03-21 16:49:53 +0900

    Add files via upload

---
+++ models_mlp.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import tensorflow.contrib.layers as tcl
+
+def lrelu(x, th=0.2, name="lrelu"):
+    return tf.maximum(th * x, x)
+	
+def generator(x, isTrain=True, reuse=False):
+	with tf.variable_scope('generator', reuse=reuse):
+	
+		output = tcl.fully_connected(x, 4 * 4 * 512, activation_fn=lrelu, normalizer_fn = tcl.batch_norm)
+		output = tcl.fully_connected(x, 64, activation_fn=lrelu, normalizer_fn = tcl.batch_norm)
+		output = tcl.fully_connected(x, 64, activation_fn=lrelu, normalizer_fn = tcl.batch_norm)
+		output = tcl.fully_connected(x, 64 * 64 * 1, activation_fn=tf.nn.tanh, normalizer_fn = tcl.batch_norm)
+		
+		return output
+
+def discriminator(x, isTrain=True, reuse=False):
+	with tf.variable_scope('discriminator', reuse=reuse):
+        
+		output = tcl.fully_connected(tcl.flatten(x), 64, activation_fn=tf.nn.relu, normalizer_fn = tcl.batch_norm)
+		output = tcl.fully_connected(output, 64, activation_fn=tf.nn.relu, normalizer_fn = tcl.batch_norm)
+		output = tcl.fully_connected(output, 64, activation_fn=tf.nn.relu, normalizer_fn = tcl.batch_norm)
+		logit = tcl.fully_connected(output, 1, activation_fn=None)
+		
+		return logit
---
+++ train_mlp.py
@@ -0,0 +1,102 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+import models_mlp as models
+
+# training parameters
+num_examples = 300
+batch_size = 32
+iters = 4
+save_every = 2
+output_dir = 'output_mlp'
+x_dim = 64 * 64
+z_dim = 128
+
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
+
+if not os.path.isdir(os.path.join(output_dir, 'checkpoints')):
+    os.makedirs(os.path.join(output_dir, 'checkpoints'))
+
+if not os.path.isdir(os.path.join(output_dir, 'samples')):
+    os.makedirs(os.path.join(output_dir, 'samples'))
+	
+    
+x = tf.placeholder(tf.float32, shape=(None, x_dim))
+z = tf.placeholder(tf.float32, shape=(None, z_dim))
+isTrain = tf.placeholder(dtype=tf.bool)
+
+fake_x = models.generator(z, isTrain)
+
+D_real = models.discriminator(x, isTrain)
+D_fake = models.discriminator(fake_x, isTrain, reuse=True)
+
+D_loss = tf.reduce_mean(D_fake) - tf.reduce_mean(D_real)
+G_loss = tf.reduce_mean(D_fake)
+
+alpha = tf.random_uniform(shape=[batch_size, z_dim], minval=0, maxval=1.)
+differences = fake_x - x
+interpolates = x + (alpha*differences)
+gradients = tf.gradients(models.discriminator(interpolates, isTrain, reuse=True), [interpolates])[0]
+slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1,2]))
+gradient_penalty = tf.reduce_mean((slopes-1.)**2)
+D_loss += 10 * gradient_penalty
+
+t_vars = tf.trainable_variables()
+D_vars = [var for var in t_vars if 'discriminator' in var.name]
+G_vars = [var for var in t_vars if 'generator' in var.name]
+
+D_train = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(D_loss, var_list=D_vars)
+G_train = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(G_loss, var_list=G_vars)
+    
+training_data = np.random.rand(num_examples, x_dim) / 0.75418890625
+#training_data = np.ones((num_examples, 64, 64, 1), dtype='float32')
+
+def inf_train_gen():
+    while True:
+        np.random.shuffle(training_data)
+        for i in range(0, num_examples - batch_size + 1, batch_size):
+            yield np.array(training_data[i:i+batch_size], dtype='float32')
+            
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+sess.close()
+
+model_saver = tf.train.Saver()
+
+# training-loop
+print('start')
+with tf.Session() as sess:
+
+	sess.run(tf.global_variables_initializer())
+	gen = inf_train_gen()
+	
+	for iteration in range(iters):
+		print(iteration)
+		
+		noise = np.random.normal(0, 1, (batch_size, z_dim))
+		batch_xs = next(gen)
+
+		_ = sess.run(D_train, feed_dict = {x: batch_xs, z: noise, isTrain: True})
+		_ = sess.run(G_train, feed_dict = {z: noise, isTrain: True})
+
+		# save data
+		if (iteration+1) % save_every == 0:
+			noise = np.random.normal(0, 1, (batch_size, z_dim))
+			samples = sess.run(fake_x, feed_dict = {z: noise, isTrain: False})
+
+			with open(os.path.join(output_dir, 'samples', 'samples_{}.txt').format(iteration+1), 'w') as f:
+				for i in range(batch_size):
+					output = samples[i]
+					output.reshape(64, 64)
+					
+					f.write(str(output) + "\n")
+
+		# save model
+		if (iteration+1) % save_every == 0:
+			model_saver.save(sess, os.path.join(output_dir, 'checkpoints', 'checkpoint_{}.ckpt').format(iteration+1))
+
+             
+print('end')
